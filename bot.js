9ab48920f84b87230a672f842d474a0cc006e168
Mike <michael@gagliano.me>
2018-09-19 17:01:54 -0400

    condensed and field tested build; features to come

--- bot.js
+++ bot.js
@@ -19,15 +19,15 @@ mongoose.connect(config.db);
 client.login(config.discord.token);
 
 client.on('ready', () => {
-    gameLogin(client);
+    scrapeComms(client);
     setInterval(function(){scrapeComms(client);}, 60000);
-    var channel = client.channels.find("name", config.discord.channel)
+    var channel = client.channels.find(x => x.name == config.discord.channel)
     channel.send("GNS is Online! \n(see !about for more)")
 });
 
 client.on('message', message => {
     if(message.content === '!about'){
-        message.channel.send('***Galactic News Service*** - **Automated Comms Notification** \n**Developer:** Demannu **Version:** .0.5')
+        message.channel.send('***Galactic News Service*** - **Automated Comms Notification** \n**Developer:** Demannu **Version:** 0.5a')
     }
 });
 
@@ -38,65 +38,61 @@ function telegramNotify(message){
     })
 }
 
-function gameLogin(client){
+function scrapeComms(client){
     request.get("http://baronsofthegalaxy.com/Default.aspx", function(error, response, body){
         var $ = cheerio.load(body);
         var viewState = $("#__VIEWSTATE").val()
         var viewGenerate = $("#__VIEWSTATEGENERATOR").val()
         var eventValid = $("#__EVENTVALIDATION").val()
+        var commsPost = []
         request.post({url: "http://baronsofthegalaxy.com/Default.aspx", form:{"__VIEWSTATE": viewState, "__VIEWSTATEGENERATOR": viewGenerate, "__EVENTVALIDATION": eventValid, "ctl00$tbHomeUsername": config.botg.user, "ctl00$tbHomePassword": config.botg.pass, "ctl00$btnLogin": "Login"}}, function(error, response, body){
             request.get("http://baronsofthegalaxy.com/login.aspx", function(error, response, body){
-                scrapeComms(client);
-            })
-        })
-    })
-}
-
-function scrapeComms(client){
-    var commsPost = []
-    request.get("http://baronsofthegalaxy.com/comms.aspx", function(error, response, body){
-        $ = cheerio.load(body)
-        var commTable = $("#ctl00_ContentPlaceHolder1_gvCommDisplayThread").find("tr").each((i, elm)=>{
-            var channel = $(elm).children().eq(2).text()
-            var title = $(elm).children().eq(4).text()
-            var replies = $(elm).children().eq(5).text()
-            var author = $(elm).children().eq(7).text()
-            var last = $(elm).children().eq(8).text()
-            commsPost.push({channel: channel, title: title, replies: replies, author:author, last:last})                        
-        })
-        commsPost.forEach(function(elm){
-            Comms.findOrCreate({channel: elm.channel, title: elm.title, author: elm.author}, function(err, comm, created){
-                var channel = client.channels.find("name",config.discord.channel)
-                if(err){
-                    console.log("ERROR: " + err)
-                }
-                if(created){
-                    if(comm.title !== undefined){
-                        telegramNotify("<b>New Comms</b> - " + comm.channel + " \n <b>" + comm.title + "</b> \n [" + comm.author + "]" )
-                        channel.send("***New Comms*** - **" + comm.channel + "** \n **Title:** " + comm.title + " \n **Author:** " + comm.author)
-                    }
-                    comm.replies = Number(elm.replies);
-                    comm.save((err)=>{
-                        if(err){
-                            console.log(err)
-                        }
+                request.get("http://baronsofthegalaxy.com/comms.aspx", function(error, response, body){
+                    $ = cheerio.load(body)
+                    var commTable = $("#ctl00_ContentPlaceHolder1_gvCommDisplayThread").find("tr").each((i, elm)=>{
+                        var channel = $(elm).children().eq(2).text()
+                        var title = $(elm).children().eq(4).text()
+                        var replies = $(elm).children().eq(5).text()
+                        var author = $(elm).children().eq(7).text()
+                        var last = $(elm).children().eq(8).text()
+                        commsPost.push({channel: channel, title: title, replies: replies, author:author, last:last})                        
                     })
-                    
-                } else {
-                    if(comm.replies && Number(comm.replies) < Number(elm.replies)){
-                        telegramNotify("<b>New Reply</b> - " + comm.channel + " \n <b>" + comm.title + "</b> [" + elm.replies + "] \n " + elm.last)
-                        channel.send("***New Reply*** - **" + comm.channel + "** \n **Title:** " + comm.title + " **[" + elm.replies + "]** \n " + elm.last)
-                        comm.replies = Number(elm.replies)
-                        comm.last = elm.last
-                        comm.save((err)=>{
+                    commsPost.forEach(function(elm){
+                        Comms.findOrCreate({channel: elm.channel, title: elm.title, author: elm.author}, function(err, comm, created){
+                            var channel = client.channels.find(x => x.name == config.discord.channel)
                             if(err){
-                                console.log("ERROR # : " + err)
+                                console.log("ERROR: " + err)
+                            }
+                            if(created){
+                                if(comm.title !== undefined){
+                                    telegramNotify("<b>New Comms</b> - " + comm.channel + " \n <b>" + comm.title + "</b> \n [" + comm.author + "]" )
+                                    channel.send("***New Comms*** - **" + comm.channel + "** \n **Title:** " + comm.title + " \n **Author:** " + comm.author)
+                                }
+                                comm.replies = Number(elm.replies);
+                                comm.save((err)=>{
+                                    if(err){
+                                        console.log(err)
+                                    }
+                                })
+                                
+                            } else {
+                                if(comm.replies && Number(comm.replies) < Number(elm.replies)){
+                                    telegramNotify("<b>New Reply</b> - " + comm.channel + " \n <b>" + comm.title + "</b> [" + elm.replies + "] \n " + elm.last)
+                                    channel.send("***New Reply*** - **" + comm.channel + "** \n **Title:** " + comm.title + " **[" + elm.replies + "]** \n " + elm.last)
+                                    comm.replies = Number(elm.replies)
+                                    comm.last = elm.last
+                                    comm.save((err)=>{
+                                        if(err){
+                                            console.log("ERROR # : " + err)
+                                        }
+                                    })
+                                } 
                             }
                         })
-                    } 
-                }
+                    })                   
+                })
             })
-        })                   
+        })
     })
 }
 
